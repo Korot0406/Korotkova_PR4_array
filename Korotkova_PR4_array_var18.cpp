6fde9de7014f7d8f8da2180d92576252a7f9f8a0
Korot0406 <ik.0406@mail.ru>
2026-03-30 18:05:47 +0300

    create menu

--- Korotkova_PR4_array_var18.cpp
+++ Korotkova_PR4_array_var18.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <ctime>
 using namespace std;
 
 // Функция проверки корректности ввода целого числа
@@ -27,3 +29,61 @@ int IntEnterNumber(string label){
     return stoi(raw_input);
 }
 
+
+void problem1(){
+
+}
+
+void problem2(){
+
+}
+
+void problem3(){
+
+}
+
+// Структура меню
+struct MenuItem{
+    string title;
+    void(*action)();
+};
+
+
+int main(){
+    setlocale(LC_ALL, "Russian");
+    map<int, MenuItem> menu = {
+        {1, {"Problem 1", problem1}},
+        {2, {"Problem 2", problem2}},
+        {3, {"Problem 3", problem3}}
+    };
+
+    int choice = 0;
+
+    while(true){
+        cout << "\nМеню:\n";
+
+        for (const auto& item : menu){
+            cout << "Task " << item.first << ". " << item.second.title << "\n";
+        }
+
+        cout << "0. Выход\n";
+
+        cin.ignore();
+        choice = IntEnterNumber("Введите номер пункта: ");
+
+        if (choice == 0){
+            cout << "© 2026 Korotkova I.S.\n";
+            break;
+        }
+
+        cout << "\n";
+
+        if (menu.find(choice) != menu.end()){
+            menu[choice].action();
+        }else{
+            cout << "Некорректный ввод.\n";
+        }
+    }
+    
+    return 0;
+}
